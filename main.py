a2da99f2e17f93fe3da17e850f072f444b4d6827
Vlad <vlad.sarana04@gmail.com>
2022-09-22 23:56:08 +0200

    added all and done diplom

--- main.py
+++ main.py
@@ -1,128 +1,171 @@
 import datetime
+import time
 import json
 from json import JSONDecodeError
 from os import path
 
+def loading():
+    time.sleep(1)
+    print(f'****************************\n'
+          f'Loading..\n'
+          f'Return to the main menu\n')
+    time.sleep(2)
 
-# def menu(user_choice=None):
-#     if user_choice == '1':
-#         add_new_user_info()
-#     elif user_choice == '2':
-#         search_user()
-#     elif user_choice == '3':
-#         calculate_user_age()
-#     print(f'****************************\n'
-#           f'1.Write new data about user\n'
-#           f'2.Search user\n'
-#           f'3.User Age\n'
-#           f'****************************\n')
+def search(value, is_null=True):
+    if value.isdigit() or is_null and not value:
+        return False
+    return True
 
+def string_valid(value, is_null=True):
+    if value.isdigit() or is_null and not value:
+        return False
+    return True
 
-def record_data():
-    print('FULL NAME')
-    name = input(' *Name: ')
-    surname = input(' *Surname: ')
-    patronymic = input(' *Patronymic: ')
-    date_of_birth = input(' *Date of birth (d-m-y): ')
-    date_of_death = input(' *Date of death (d-m-y or "-"): ')
-    sex = input(' *Sex: ')
-    input_data = {'Name': name, 'Surname': surname, 'Patronymic': patronymic, 'Date of birth': date_of_birth, 'Date of death': date_of_death, 'Sex': sex}
+def gender_valid(sex):
+    sex_choices = ['male', 'female', 'other', 'm', 'f', 'o', 'м', 'ж']
+    if sex.lower() in sex_choices:
+        return True
 
-    if path.isfile('user_data.json') is False:
-        f = open("user_data.json", "x")
-        json.dump([], f)
-        f.close()
-    else:
-        try:
-            with open("user_data.json", mode='r', encoding='utf-8') as f:
-                feeds = json.load(f)
+    return False
 
-            if feeds is dict:
-                feeds = [feeds]
-
-            with open("user_data.json", mode='w') as feedsjson:
-                feeds.append(input_data)
-                json.dump(feeds, feedsjson)
-
-        except JSONDecodeError:
-            f = open("user_data.json", "w")
-            json.dump([], f)
-            f.close()
-
-def find_data():
-    search_line = input('Enter the full name or part of the name: ')
-    persons = json.load(open('user_data.json'))
-
-    for person in persons:
-        person_datas = person.values()
-        for person_info in person_datas:
-            if search_line.lower() in person_info.lower():
-                print(person)
-
-def change_and_validate_date(date_in_text):
-    date_in_text = date_in_text.replace('.', '/')  # 12/10/2000
-    splitted_date = date_in_text.split('/')  # ['12', '10', '2000']
+def date_valid(entry_date):
+    entry_date = entry_date.replace('.', '/').replace(',', '/').replace('-', '/').replace(' ', '/')
     validate_result = {'status': True}
+
     try:
-        date = datetime.datetime.strptime(date_in_text, "%d/%m/%Y")
+        date = datetime.datetime.strptime(entry_date, "%d/%m/%Y")
         validate_result.update({'date': date})
-    except:
+    except Exception:
         validate_result.update({'status': False, 'error': 'Not valid date!'})
         return validate_result
-    for number in splitted_date:
-        try:
-            number = int(number)
-        except:
-            validate_result.update({'status': False, 'error': 'Not valid date!'})
-            break
 
     return validate_result
 
+def write_data_to_json(input_data):
+    try:
+        with open('user_data.json', mode='r', encoding='utf-8') as f:
+            feeds = json.load(f)
 
-def calculate_user_age():
+        if feeds is dict:
+            feeds = [feeds]
+
+        with open('user_data.json', mode='w', encoding='utf-8') as feedsjson:
+            feeds.append(input_data)
+            json.dump(feeds, feedsjson)
+
+    except JSONDecodeError:
+        f = open('user_data.json', 'w')
+        json.dump([], f)
+        f.close()
+
+def record_data():
+    print('FULL NAME')
+    name = input('  *Name: ')
+    while not string_valid(name):
+        print(f'The name "{name}" is incorrectly, please try again..')
+        name = input('\t*Name: ')
+
+    surname = input('  *Surname: ')
+    while not string_valid(surname, is_null=False):
+        print(f'The surname "{surname}" is incorrectly, please try again..')
+        surname = input('\t*Surname: ')
+
+    patronymic = input('  *Patronymic: ')
+    while not string_valid(patronymic, is_null=False):
+        print(f'The patronymic "{patronymic}" is incorrectly, please try again..')
+        patronymic = input('\t*Patronymic: ')
+
+    date_of_birth = input('*Date of birth (d-m-y): ')
+    while not date_valid(date_of_birth)['status']:
+        print(f'Date of birth "{date_of_birth}" is incorrectly, please try again..')
+        date_of_birth = input('*Date of birth (d-m-y): ')
+
+    date_of_death = input('*Date of death (d-m-y or "-"): ')
+    if date_of_death in ('', '-'):
+        pass
+    else:
+        while not date_valid(date_of_death)['status']:
+            if date_of_death in ('', '-'):
+                print(f'Date of death "{date_of_death}" is incorrectly, please try again..')
+                date_of_death = input('*Date of death (d-m-y): ')
+
+    sex = input('*Sex (male/female/other): ')
+    while not gender_valid(sex):
+        print(f'The gender "{sex}" is incorrectly, please try again')
+        sex = input('*Sex (male/female/other): ')
+
+    input_data = {'Name': name, 'Surname': surname, 'Patronymic': patronymic,
+                  'Date of birth': date_of_birth, 'Date of death': date_of_death, 'Sex': sex}
+
+    if path.isfile('user_data.json') is False:
+        f = open('user_data.json', 'x')
+        json.dump([], f)
+        f.close()
+        write_data_to_json(input_data)
+    else:
+        write_data_to_json(input_data)
+
+    loading()
+
+def find_data():
+    search_line = input('Enter the full name or part of the name: ')
+    while not search(search_line):
+        print(f'The value "{search_line}" is incorrectly, please try again..')
+        search_line = input('Enter the full name or part of the name: ')
+
+    try:
+        persons = json.load(open('user_data.json', encoding='utf-8'))
+        for person in persons:
+            person_data = person.values()
+            for person_info in person_data:
+                if search_line.lower() in person_info.lower():
+                    print(person)
+                    break
+            else:
+                print('Found: 0')
+    except FileNotFoundError:
+        print('! First, create a file \n')
+
+    loading()
+
+def calculate_person_age():
     date = input('Choose your date: ')
-    validate_result = change_and_validate_date(date)
+    validate_result = date_valid(date)
     if validate_result['status']:
         now_date = datetime.datetime.now()
         user_age = now_date.year - validate_result['date'].year
         if now_date.month < validate_result['date'].month or \
-                now_date.month == validate_result['date'].month and\
+                now_date.month == validate_result['date'].month and \
                 now_date.day < validate_result['date'].day:
             user_age -= 1
-
-        print(user_age)
+        print(f'Age: {user_age} \n')
     else:
         print(validate_result['error'])
 
+    loading()
 
+call_function = {1: record_data, 2: find_data, 3: calculate_person_age}
 
+def main():
+    while True:
 
-# user_choice = 1
-# menu()
-# while user_choice != 0:
-#     user_choice = input('Enter your choice: ')
-#     menu(user_choice)
+        print(f'****************************\n'
+              f'1.Write information about a person\n'
+              f'2.Get information about a person\n'
+              f'3.Find out the age of a person\n'
+              f'4.Exit\n'
+              f'****************************\n')
 
-call_function = {1: record_data, 2: find_data, 3: calculate_user_age}
+        choice = input('Make your choice: ')
 
-while True:
+        if not choice.isdigit() or int(choice) > 4 or not choice:
+            print('! Input error, please try again.. \n')
+            continue
 
-    print(f'****************************\n'
-          f'1.Write information about a person\n'
-          f'2.Get information about a person\n'
-          f'3.Find out the age of a person\n'
-          f'4.Exit\n'
-          f'****************************\n')
-
-    choice = input('Make your choice: ')
-
-    if not choice.isdigit() or int(choice) > 4:
-        print('Input error, please try again..')
-        continue
-
-    choice_int = int(choice)
-    if choice_int == 4:
-        print('Goodbye...')
-        break
-    else:
-        call_function[choice_int]()
+        choice_int = int(choice)
+        if choice_int == 4:
+            print('Goodbye...')
+            break
+        else:
+            call_function[choice_int]()
+main()
